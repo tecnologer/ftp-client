d1fcdf60a8796a6e6ac8d72f24603ad25f196125
Rey David <rdominguez@tecnologer.net>
2021-01-13 23:11:00 -0600

    test list local files in path as TreeElement

---
+++ test/files_test.go
@@ -0,0 +1,20 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/tecnologer/ftp-v2/src/models/files"
+	"github.com/tecnologer/ftp-v2/src/models/tools"
+)
+
+func TestListFiles(t *testing.T) {
+	path := "/home/tecnologer/java"
+
+	files, err := files.ListFiles(path)
+
+	if err != nil {
+		t.Fail()
+	}
+
+	tools.PrintTree(files, 0)
+}
